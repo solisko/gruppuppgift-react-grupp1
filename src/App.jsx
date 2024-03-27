09e1906d49f835bf00cc653580ec9699caf3caaa
Emma Andersson <anderssons-emma@hotmail.com>
2024-03-27 14:21:23 +0100

    Test header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './Header/Header'
 
 function App() {
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,9 @@
+function Header(
+    return(
+        <div style={{height: '100px', backgroundColor: 'brown'}}>
+        
+        </div>
+    )
+)
+
+export default Header;
