5fa5b6e6800ab40262af25136a9af07b5e592050
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-27 14:27:11 +0100

    fetch all start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import FetchAuctions from "./components/FetchAuctions/FetchAuctions";
 
 function App() {
-
   return (
     <>
-
+      <FetchAuctions />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
