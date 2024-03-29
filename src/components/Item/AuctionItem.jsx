01b88628115257d5bed85a4999a84633bda1fd0c
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-29 21:50:01 +0100

    br add

--- src/components/Item/AuctionItem.jsx
+++ src/components/Item/AuctionItem.jsx
@@ -3,7 +3,10 @@ import styles from "../Item/auctionitem.module.css";
 const formatDateTime = (dateTimeStr) => {
   const dateTime = new Date(dateTimeStr);
   const formattedDate = dateTime.toLocaleDateString();
-  const formattedTime = dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const formattedTime = dateTime.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return `${formattedDate}, kl. ${formattedTime}`;
 };
 
@@ -15,8 +18,16 @@ export default function AuctionItem({ auctions }) {
           <div className={styles.auctionCard} key={index}>
             <h2>{auction.Title}</h2>
             <p>{auction.Description}</p>
-            <p>Starttid budgivning: {formatDateTime(auction.StartDate)} </p>
-            <p>Sluttid budgivning: {formatDateTime(auction.EndDate)} </p>
+            <p>
+              Starttid budgivning:
+              <br />
+              {formatDateTime(auction.StartDate)}
+            </p>
+            <p>
+              Sluttid budgivning:
+              <br />
+              {formatDateTime(auction.EndDate)}
+            </p>
             <h3>Start pris: {auction.StartingPrice}</h3>
             <p>Upplagd av {auction.CreatedBy}</p>
           </div>
