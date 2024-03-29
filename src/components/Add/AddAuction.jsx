47e8ed61eedc0f1937ff1513422d956c7df0e13d
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-30 00:09:23 +0100

    form comming together

--- src/components/Add/AddAuction.jsx
+++ src/components/Add/AddAuction.jsx
@@ -4,6 +4,10 @@ import styles from "../Add/addauction.module.css";
 export default function AddAuction() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [startingPrice, setStartingPrice] = useState("");
+  const [createdBy, setCreatedBy] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,6 +25,23 @@ export default function AddAuction() {
             value={description}
           />
         </label>
+        <label htmlFor="startDate">
+          Starttid:
+          <input type="text" value={startDate} />
+        </label>
+        <label htmlFor="endDate">
+          Sluttid:
+          <input type="text" value={endDate} />
+        </label>
+        <label htmlFor="startprice">
+          Start pris:
+          <input type="number" placeholder="Första budet blir..." value={startingPrice} />
+        </label>
+        <label htmlFor="creator">
+          Skapad av:
+          <input type="text" placeholder="Skriv ditt namn..." value={createdBy} />
+        </label>
+        <button type="submit">Skapa auktion</button>
       </form>
     </div>
   );
