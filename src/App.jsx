473277da9d9122d41e0f2d6d95e956343e603d56
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-30 00:32:00 +0100

    removed addcomp from app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import Navigation from "./components/Header/Navigation";
 import Switch from "./components/Header/Switch";
 import ListAuctions from "./components/List/ListAuctions";
-import AddAuction from "./components/Add/AddAuction";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
             <Switch />
           </BrowserRouter>
           <div className="mainContent" >
-            <AddAuction/>
             <ListAuctions />
           </div>
         </AuctionContextProvider>
