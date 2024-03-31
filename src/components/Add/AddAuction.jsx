187729dcc9e75f4236ffdde2b4318304369c4447
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-31 18:04:04 +0200

    auction date fix

--- src/components/Add/AddAuction.jsx
+++ src/components/Add/AddAuction.jsx
@@ -9,6 +9,17 @@ export default function AddAuction() {
   const [StartingPrice, setStartingPrice] = useState("");
   const [CreatedBy, setCreatedBy] = useState("");
 
+  const getCurrentDateTime = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = `${now.getMonth() + 1}`.padStart(2, "0");
+    const day = `${now.getDate()}`.padStart(2, "0");
+    const hours = `${now.getHours()}`.padStart(2, "0");
+    const minutes = `${now.getMinutes()}`.padStart(2, "0");
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -75,6 +86,7 @@ export default function AddAuction() {
           <input
             type="datetime-local"
             value={StartDate}
+            min={getCurrentDateTime()}
             onChange={(e) => setStartDate(e.target.value)}
             required
           />
