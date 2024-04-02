e4c16d2479b7a987d692290387d677f0df56ed1f
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-02 21:15:46 +0200

    details small fixes

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -1,5 +1,41 @@
 export default function ItemDetails() {
   return (
-    <div>ItemDetails</div>
-  )
+    <div>
+      <div>
+        <h1>{auctionID.Title}</h1>
+        {/* <img className={styles.image} src="" alt="" /> */}
+        <h3>{auctionID.Description}</h3>
+        {/* <p>
+          Starttid budgivning:
+          <br />
+          {formatDateTime(auction.StartDate)}
+        </p>
+        <p>
+          Sluttid budgivning:
+          <br />
+          {formatDateTime(auction.EndDate)}
+        </p> */}
+        <h3>Start pris: {auctionID.StartingPrice}</h3>
+        <button>Lägg nytt bud</button>
+        <section>
+          <h2>Alla bud:</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Bud</th>
+                <th>Budgivare</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>300</td>
+                <td>Sofia</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <p>Upplagd av {auctionID.CreatedBy}</p>
+      </div>
+    </div>
+  );
 }
