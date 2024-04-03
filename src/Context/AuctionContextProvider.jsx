5cf794ef85aa13f50ababfc170b794d93454aa5c
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-03 14:33:10 +0200

    updated url

--- src/Context/AuctionContextProvider.jsx
+++ src/Context/AuctionContextProvider.jsx
@@ -5,35 +5,37 @@ export const AuctionContext = createContext();
 const AuctionProvider = (props) => {
   const [auctions, setAuctions] = useState([]);
 
-  
-    const fetchAuctions = async (searchTerm = '') => {
-      try {
-        const response = await fetch(`https://auctioneer.azurewebsites.net/auction/1zyx`);
-        if (!response.ok) {
-          throw new Error("Failed to fetch auctions");
-        }
-        let data = await response.json();
-
-        
-
-        if(!searchTerm) {
-          const currentDatetime = new Date();
-          data = data.filter(auction => new Date(auction.EndDate) > currentDatetime);
-        }
-        
-        if (searchTerm) {
-          data = data.filter(auction => auction.Title.toLowerCase().includes(searchTerm.toLowerCase()));
-        }
-
-        setAuctions(data);
-      } catch (error) {
-        console.error("Error fetching auctions", error);
+  const fetchAuctions = async (searchTerm = "") => {
+    let url = "https://auctioneer.azurewebsites.net/auction/1zyx";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch auctions");
       }
-    };
+      let data = await response.json();
 
-    useEffect(() => {
-      fetchAuctions();
-    },[]);
+      if (!searchTerm) {
+        const currentDatetime = new Date();
+        data = data.filter(
+          (auction) => new Date(auction.EndDate) > currentDatetime
+        );
+      }
+
+      if (searchTerm) {
+        data = data.filter((auction) =>
+          auction.Title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      }
+
+      setAuctions(data);
+    } catch (error) {
+      console.error("Error fetching auctions", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAuctions();
+  }, []);
 
   return (
     <AuctionContext.Provider value={{ auctions, fetchAuctions }}>
