f66a1a068fcb104fe81aef637df400b82f260ea4
Tomas Yacoub <tomasyacoub@hotmail.com>
2024-04-05 01:33:33 +0200

    temporary

--- src/components/Footern/Footer.jsx
+++ src/components/Footern/Footer.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import styles from "./footer.module.css"
 
-
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="styles.footer">
-      <div className="container">
-        <p>FOOTER</p>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.contactInfo}>
+          <h3>Kontakt</h3>
+          <ul>
+            <li>Telefon: 08-910 568 10</li>
+            <li>Adress: Isafjordsgatan 30A</li>
+            <li>E-post: Auctioneers@gmail.se</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
