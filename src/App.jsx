e7c57693e42ce23d90501c7fa33f29d7b08540fc
Emma Andersson <anderssons-emma@hotmail.com>
2024-04-10 18:19:00 +0200

    trytofixcontext

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Navigation from "./components/Header/Navigation";
 import Switch from "./components/Header/Switch";
 import Footer from "./components/Footern/Footer";
 import SearchBar from "./components/Search/SearchBar";
+import Admin from "./components/Admin/Admin";
+import AuctionRow from "./components/Admin/AuctionRow";
 
 function App() {
   return (
