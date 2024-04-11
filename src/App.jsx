aee417bbb9592251115cbd46e58ccd8e3e9e3d80
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-11 13:45:44 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Navigation from "./components/Header/Navigation";
 import Switch from "./components/Header/Switch";
 import Footer from "./components/Footern/Footer";
 import SearchBar from "./components/Search/SearchBar";
-import Admin from "./components/Admin/Admin";
-import AuctionRow from "./components/Admin/AuctionRow";
 
 function App() {
   return (
