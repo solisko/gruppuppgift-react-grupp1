16b7c1cf14eb5b53e8222bd7fcc7dade62a8a86e
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-16 18:01:29 +0200

    restored itemdetails

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -1,61 +1,3 @@
-import { useContext, useEffect } from "react";
-import { AuctionContext } from "../../Context/AuctionContextProvider";
-import { useLocation } from "react-router-dom";
-import styles from "../ItemDetails/itemdetails.module.css";
-import AddBid from "../Add/AddBid";
-
-export default function ItemDetails() {
-  const { auctions, bids, fetchBidsByAuctionId } = useContext(AuctionContext);
-  const location = useLocation();
-  const auction = auctions.filter(
-    (auct) => auct.AuctionID === location.state.id
-  )[0];
-
-  useEffect(() => {
-    if (auction) {
-      fetchBidsByAuctionId(auction.AuctionID);
-    }
-  }, [auction, fetchBidsByAuctionId]);
-
-  return (
-    <>
-      {auction && (
-        <div className={styles.detailsContainer}>
-          <img className={styles.image} src="" alt="" />
-          <h3>{auction.Description}</h3>
-          <section className={styles.bidsSection}>
-            <AddBid auction={auction} />
-            {bids && bids.length > 0 ? (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Bud</th>
-                    <th>Budgivare</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {bids
-                    .slice()
-                    .reverse()
-                    .map((bid, index) => (
-                      <tr key={index}>
-                        <td>{bid.Amount}</td>
-                        <td>{bid.Bidder}</td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            ) : (
-              <h2>Inga Bud än</h2>
-            )}
-          </section>
-          <p>Upplagd av {auction.CreatedBy}</p>
-        </div>
-      )}
-    </>
-  );
-}
-
 import { useContext, useEffect, useState } from "react";
 import { AuctionContext } from "../../Context/AuctionContextProvider";
 import { useLocation } from "react-router-dom";
@@ -85,7 +27,6 @@ export default function ItemDetails() {
     }
   }, [auction]);
 
-<<<<<<<<< Temporary merge branch 1
   useEffect(() => {
     if (bids && bids.length > 0) {
       const winningBid = bids.reduce((prev, current) =>
@@ -127,8 +68,6 @@ export default function ItemDetails() {
     fetchBidsByAuctionId(auctionId);
   };
 
-=========
->>>>>>>>> Temporary merge branch 2
   return (
     <>
       {auction && (
@@ -138,7 +77,6 @@ export default function ItemDetails() {
           <h2>{auction.Title}</h2>
           <h3>{auction.Description}</h3>
           <p>Upplagd av {auction.CreatedBy}</p>
-<<<<<<<<< Temporary merge branch 1
           <h3>Start pris: {auction.StartingPrice} SEK</h3>
           {isEnded ? (
             <>
@@ -194,8 +132,6 @@ export default function ItemDetails() {
               )}
             </section>
           )}
-=========
->>>>>>>>> Temporary merge branch 2
         </div>
       )}
     </>
